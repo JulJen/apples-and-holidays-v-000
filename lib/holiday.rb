55a4a5ab4fb73d8edf0bf1c9453233f4741f3d91
Julia J-Prog <juliajen.prog@gmail.com>
2017-12-30 23:26:51 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -52,7 +52,7 @@ end
 
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
-  holiday_hash[:summer].values.flatten
+  holiday_hash[:winter].values.flatten
 end
 
 def all_supplies_in_holidays(holiday_hash)
@@ -81,12 +81,10 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-  puts "#{holiday_name}"
-end
   # holiday_hash.each do |season, holiday_details_hash|
-    # puts "#{holiday_details_hash.keys.select {|key| key.to_s.include?("BBQ")}}"
-#   end
-# end
+  #   puts "#{holiday_details_hash.keys.select {|key| key.to_s.include?("BBQ")}}"
+  end
+end
 
     # holiday_details_hash.each do |holiday_name, holiday_supplies|
   # holiday_hash.each { |key,value| puts "#{key}".join(" ") if value == "BBQ" }
