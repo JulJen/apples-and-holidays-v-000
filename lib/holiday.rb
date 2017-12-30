3e42b218b74933fff00a69b9439a7b40d72811b7
Julia J-Prog <juliajen.prog@gmail.com>
2017-12-30 23:38:20 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -85,7 +85,7 @@ def all_holidays_with_bbq(holiday_hash)
   new_array = Array.new
   holiday_hash.each do |season, holiday_details_hash|
     holiday_details_hash.each do |holiday_name, holiday_supplies|
-      if holiday_supplies.select("BBQ")
+      if holiday_supplies.include?("BBQ")
         new_array << holiday_name
       end
     end
